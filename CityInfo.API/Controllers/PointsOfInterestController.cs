e02ddaf959bfd1719defe2a4365fa6a11532ee8c
Max Coyle <Max.Coyle@kalibrate.com>
2018-05-11 10:34:37 +0100

    resharper suggestions

--- CityInfo.API/Controllers/PointsOfInterestController.cs
+++ CityInfo.API/Controllers/PointsOfInterestController.cs
@@ -1,19 +1,18 @@
-﻿using System;
-using System.Linq;
-using CityInfo.API.Models;
+﻿using CityInfo.API.Models;
 using CityInfo.API.Services;
 using Microsoft.AspNetCore.JsonPatch;
 using Microsoft.AspNetCore.Mvc;
-using Microsoft.AspNetCore.Mvc.ModelBinding;
 using Microsoft.Extensions.Logging;
+using System;
+using System.Linq;
 
 namespace CityInfo.API.Controllers
 {
     [Route("api/cities")]
     public class PointsOfInterestController : Controller
     {
-        private ILogger<PointsOfInterestController> _logger;
-        private IMailService _mailService;
+        private readonly ILogger<PointsOfInterestController> _logger;
+        private readonly IMailService _mailService;
 
         public PointsOfInterestController(ILogger<PointsOfInterestController> logger, IMailService mailService)
         {
@@ -28,13 +27,9 @@ namespace CityInfo.API.Controllers
             {
                 var city = CitiesDataStore.Current.Cities.FirstOrDefault(c => c.Id == cityId);
 
-                if (city == null)
-                {
-                    _logger.LogInformation($"city with id {cityId} wasn't found when accessing points of interest.");
-                    return NotFound();
-                }
-
-                return Ok(city.PointsOfInterest);
+                if (city != null) return Ok(city.PointsOfInterest);
+                _logger.LogInformation($"city with id {cityId} wasn't found when accessing points of interest.");
+                return NotFound();
 
             }
             catch (Exception ex)
